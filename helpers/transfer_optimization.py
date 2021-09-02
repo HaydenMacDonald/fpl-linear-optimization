3a1eba49adc1ba88624d8c0ee803b43a22d2a75b
Hayden MacDonald <hayden.macdonald.8778@gmail.com>
2021-09-02 21:05:01 +0100

    fix: uncommented code

--- helpers/transfer_optimization.py
+++ helpers/transfer_optimization.py
@@ -28,22 +28,21 @@ def transfer_optimization(current_team, data_file, analysis):
 
     ## Multitransfer horizon method
     elif analysis == "multitransfer":
-        # HORIZON = 4
-        # multi_score_forecast = pd.DataFrame({"week_{}".format(i): df["total_points"] / 38 for i in range(HORIZON)})
-        # multi_score_forecast.head()
+        HORIZON = 4
+        multi_score_forecast = pd.DataFrame({"week_{}".format(i): df["total_points"] / 38 for i in range(HORIZON)})
+        multi_score_forecast.head()
 
 
-        # ## Instantiate MultiHorizonTransferOptimiser class
-        # opt = MultiHorizonTransferOptimiser(multi_score_forecast.values.T, buy_prices.values, sell_prices.values, positions.values, clubs.values, 4)
+        ## Instantiate MultiHorizonTransferOptimiser class
+        opt = MultiHorizonTransferOptimiser(multi_score_forecast.values.T, buy_prices.values, sell_prices.values, positions.values, clubs.values, 4)
 
-        # ## Run solver
-        # transfer_in_decisions, transfer_out_decisions, starters, sub_decisions, captain_decisions = opt.solve(current_squad_indices, budget_now=0, sub_factor=0.2)
+        ## Run solver
+        transfer_in_decisions, transfer_out_decisions, starters, sub_decisions, captain_decisions = opt.solve(current_squad_indices, budget_now=0, sub_factor=0.2)
 
-        # for week in range(len(transfer_in_decisions)):
-        #     print("Week {}".format(week))
-        #     for i in range(len(transfer_in_decisions[week])):
-        #         if transfer_in_decisions[week][i].value() == 1:
-        #             print("Transferred in: {} {} {}".format(names[i], buy_prices[i], multi_score_forecast.values.T[week][i]))
-        #         if transfer_out_decisions[week][i].value() == 1:
-        #             print("Transferred out: {} {} {}".format(names[i], sell_prices[i], multi_score_forecast.values.T[week][i]))
-        pass
+        for week in range(len(transfer_in_decisions)):
+            print("Week {}".format(week))
+            for i in range(len(transfer_in_decisions[week])):
+                if transfer_in_decisions[week][i].value() == 1:
+                    print("Transferred in: {} {} {}".format(names[i], buy_prices[i], multi_score_forecast.values.T[week][i]))
+                if transfer_out_decisions[week][i].value() == 1:
+                    print("Transferred out: {} {} {}".format(names[i], sell_prices[i], multi_score_forecast.values.T[week][i]))
